255d15f947308b6ce4605fbb81314a81539836bc
JPromi <jonasmpr05@gmail.com>
2023-09-18 13:31:37 +0200

    add autificator

--- event-manager/src/app/app.component.ts
+++ event-manager/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Session } from './dtos/auth/session';
+import { ActivatedRoute } from '@angular/router';
+import cfg from './config.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'event-manager';
+
+  public loggedIn = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: ActivatedRoute,
+  ){}
+
+  ngOnInit() {
+    this.authService.session().subscribe(
+      (session: Session) => {
+
+        if(session.loggedIn) {
+          this.loggedIn = true;
+        } else {
+          this.router.data.subscribe(
+            data => {
+              if(!data["public"]) {
+                window.location.href = cfg.domain.auth;
+              }
+            }
+          );
+        }
+
+      }
+    )
+  }
+
+  checkUserLogin() {
+    
+  }
 }
