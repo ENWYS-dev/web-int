74c4990dc83a75c1f350792a777c112e4da46f66
JPromi <jonasmpr05@gmail.com>
2023-09-18 19:25:21 +0200

    remove ,

--- event-manager/src/app/app-routing.module.ts
+++ event-manager/src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, },
-  { path: 'event', component: HomeComponent, },
-  { path: 'material', component: HomeComponent, },
+  { path: '', component: HomeComponent },
+  { path: 'event', component: HomeComponent },
+  { path: 'material', component: HomeComponent },
 ];
 
 @NgModule({
