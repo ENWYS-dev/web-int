a9494968fd022140ddbdb1ae31f22ac26525487c
JPromi <jonasmpr05@gmail.com>
2023-09-18 12:49:59 +0200

    add auth service

---
+++ event-manager/src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Session } from '../dtos/auth/session';
+import { Observable } from 'rxjs';
+import cfg from '../config.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = cfg.api + 'auth';
+  
+  private httpOptions = {
+    withCredentials: true
+  };
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  session(type: string = '') : Observable<Session> {
+    var typeLink = '';
+    if(type) {
+      typeLink = '?type=' + type;
+    }
+    return this.httpClient.get<Session>(this.url + '/session' + typeLink, this.httpOptions);
+  }
+}
