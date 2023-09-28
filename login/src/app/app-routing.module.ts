560fb529c7f4184b2e43725ea369b1ed4b8070ce
JPromi <jonasmpr05@gmail.com>
2023-09-28 11:19:26 +0200

    remove data

--- login/src/app/app-routing.module.ts
+++ login/src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { LogoutComponent } from './components/logout/logout.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginContainerComponent, data :{ type: 'login' } },
-  { path: 'logout', component: LogoutComponent },
-  { path: 'redirect', component: LoginContainerComponent, data :{ type: 'redirect' } },
-  { path: 'password-reset', component: LoginContainerComponent, data :{ type: 'password-reset' } }
+  { path: 'login', component: LoginContainerComponent },
+  { path: 'logout', component: LoginContainerComponent },
+  { path: 'redirect', component: LoginContainerComponent },
+  { path: 'password-reset', component: LoginContainerComponent }
 ];
 
 @NgModule({
